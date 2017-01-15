fa32dd91e0ad440336937d278e0e848d0b8cfbb6
Naoaki Hashimoto <hasimoto0130gt@gmail.com>
2017-01-15 18:23:03 +0900

    fix

--- src/main/java/com/example/LineBotController.java
+++ src/main/java/com/example/LineBotController.java
@@ -357,7 +357,7 @@ public class LineBotController {
                 log.info("Returns echo message {}: {}", replyToken, "google");
                 this.replyText(
                         replyToken,
-                        text
+                        "google"
                 );
                 break;
             default:
